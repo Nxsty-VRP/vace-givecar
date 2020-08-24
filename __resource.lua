9e6c36a0b77f0148ab94cfbc20004d9d35f4b5c2
Nxsty-VRP <69446530+Nxsty-VRP@users.noreply.github.com>
2020-08-25 00:05:26 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,6 +1,6 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-description 'ESX Nuo Give Car'
+description 'vace-givecar'
 
 version '1.0.0'
 
@@ -18,4 +18,3 @@ client_scripts {
 dependency 'es_extended'
 
 export 'GeneratePlate'
-client_script "VBEAkXoBYinkfCGdAH.lua"
